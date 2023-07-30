59032a50d2d49f3a6025cb6fcb1e9f1d4e9d99ca
arosspope <andrew.pope456@gmail.com>
2023-07-30 17:12:35 +1000

    wip: cfg input wifi ssid/psk, led (wrong hardware)

--- build.rs
+++ build.rs
@@ -1,5 +1,18 @@
+#[toml_cfg::toml_config]
+pub struct Config {
+    #[default("")]
+    wifi_ssid: &'static str,
+    #[default("")]
+    wifi_psk: &'static str,
+}
+
 // Necessary because of this issue: https://github.com/rust-lang/cargo/issues/9641
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Check if the `cfg.toml` file exists and has been filled out.
+    if !std::path::Path::new("cfg.toml").exists() {
+        panic!("You need to create a `cfg.toml` file with your Wi-Fi credentials!");
+    }
+
     embuild::build::CfgArgs::output_propagated("ESP_IDF")?;
     embuild::build::LinkArgs::output_propagated("ESP_IDF")?;
     Ok(())
